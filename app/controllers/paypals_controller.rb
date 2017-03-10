227627a6655ad3fd7829f04ed5dac2706c41f292
Joe-noh <goflb.jh@gmail.com>
2017-03-11 00:16:48 +0900

    confirmation page -> paypal login

--- app/controllers/paypals_controller.rb
+++ app/controllers/paypals_controller.rb
@@ -1,5 +1,45 @@
 class PaypalsController < ApplicationController
   def create
+    api = PayPal::SDK::Merchant::API.new
+
+    @set_express_checkout = api.build_set_express_checkout(
+      SetExpressCheckoutRequestDetails: {
+        ReturnURL: "http://localhost:3000/payment?nonce=123",
+        CancelURL: "http://localhost:3000/",
+        PaymentDetails: [{
+          OrderTotal: {
+            currencyID: "JPY",
+            value: params[:price].inject(:+)
+          },
+          ItemTotal: {
+            currencyID: "JPY",
+            value: params[:price].inject(:+)
+          },
+          ShippingTotal: {
+            currencyID: "JPY",
+            value: "0"
+          },
+          TaxTotal: {
+            currencyID: "JPY",
+            value: "0"
+          },
+          PaymentDetailsItem: [{
+            Name: params[:name][0],
+            Quantity: 1,
+            Amount: {
+              currencyID: "JPY",
+              value: params[:price][0]
+            },
+            ItemCategory: "Physical"
+          }],
+          PaymentAction: "Sale"
+        }]
+      }
+    )
+
+    @set_express_checkout_response = api.set_express_checkout(@set_express_checkout)
+
+    redirect_to api.express_checkout_url(@set_express_checkout_response)
   end
 
   def show
