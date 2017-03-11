f23df136e6db4bef0ef71371ac0fd9e5ac77e2a8
Joe-noh <goflb.jh@gmail.com>
2017-03-11 19:22:57 +0900

    confirmation page -> execute checkout

--- app/controllers/paypals_controller.rb
+++ app/controllers/paypals_controller.rb
@@ -1,7 +1,5 @@
 class PaypalsController < ApplicationController
   def create
-    api = PayPal::SDK::Merchant::API.new
-
     @set_express_checkout = api.build_set_express_checkout(
       SetExpressCheckoutRequestDetails: {
         ReturnURL: "http://localhost:3000/payment?nonce=123",
@@ -43,8 +41,45 @@ class PaypalsController < ApplicationController
   end
 
   def show
+    @token = params[:token]
+    @payer_id = params[:PayerID]
+
+    @get_express_checkout = api.build_get_express_checkout_details({
+      Token: @token
+    })
+
+    @get_express_checkout_details_response = api.get_express_checkout_details(@get_express_checkout)
   end
 
   def execute
+    @do_express_checkout_payment = api.build_do_express_checkout_payment({
+      DoExpressCheckoutPaymentRequestDetails: {
+        PaymentAction: "Sale",
+        Token: params[:token],
+        PayerID: params[:payer_id],
+        PaymentDetails: [{
+          OrderTotal: {
+            currencyID: "JPY",
+            value: 300
+          }
+        }]
+      }
+    })
+
+    @do_express_checkout_payment_response = @api.do_express_checkout_payment(@do_express_checkout_payment)
+
+    if @do_express_checkout_payment_response.success?
+      details = @do_express_checkout_payment_response.DoExpressCheckoutPaymentResponseDetails
+      p details
+    else
+      errors = @do_express_checkout_payment_response.Errors
+      p errors
+    end
+  end
+
+  private
+
+  def api
+    @api ||= PayPal::SDK::Merchant::API.new
   end
 end
